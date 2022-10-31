2e6f2f9c7f144d0f9066df0336200cf8ff4ba32d
Bishnu Prasad Muduli <100763977+bmuduli@users.noreply.github.com>
2022-10-31 15:37:01 +0530

    Add files via upload

---
+++ Banking.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class InsufficeientAmountException extends Exception{
+    
+    InsufficeientAmountException(String msg){
+        super(msg);
+    }
+}
+
+class InvalidAmountException extends Exception{
+    
+    InvalidAmountException(String msg1){
+        super(msg1);
+    }
+}
+
+public class Banking{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+            int TotalAmount=75000;
+            int withdrawAmount=sc.nextInt();
+        try{
+            
+            try{
+                if(withdrawAmount<=0){
+                    throw new InvalidAmountException("Invalid amount Entered");
+                }
+            }
+                catch(InvalidAmountException iv){
+                     iv.printStackTrace();
+            }
+            
+            if(withdrawAmount > 0){
+                if(withdrawAmount >= TotalAmount){
+                    throw new InsufficeientAmountException("Insuffient amount avalilable in your Bank");
+                }
+                else{
+                    System.out.println("Amount "+withdrawAmount+" withdraw Sucessfully");
+                    TotalAmount =  TotalAmount- withdrawAmount;   
+                } 
+            }
+        }
+        catch(InsufficeientAmountException e){
+             e.printStackTrace();
+        }
+        finally{
+             System.out.println("Available Balance is : "+TotalAmount);
+        }
+     }
+}
